6ddcdff6572f07878ad60cb7ea615961e492028e
Tao Wen <taowen@gmail.com>
2018-03-04 14:40:52 +0800

    explicit specify valid addressing modes

--- instruction.go
+++ instruction.go
@@ -1,8 +1,10 @@
 package amd64
 
+import "errors"
+
 type opcode byte
 
-type variants map[condition]*instruction
+type variants map[[2]condition]*instruction
 type overrides map[string]interface{}
 
 type instruction struct {
@@ -10,19 +12,34 @@ type instruction struct {
 	// if not match variant, this opcode will be used by default
 	opcode    opcode
 	assemble  interface{}
-	variants  map[condition]*instruction
+	variants  map[[2]condition]*instruction
 	overrides map[string]interface{}
 }
 
 type condition struct {
-	rm byte // register or memory size
+	r byte // only register, size
+	m byte // only memory, size
+	rm byte // register or register based memory, size
+}
+
+func (insn *instruction) initVariants() {
+	for _, variant := range insn.variants {
+		if variant.opcode == 0 {
+			variant.opcode = insn.opcode
+		}
+		if variant.overrides == nil {
+			variant.overrides = insn.overrides
+		}
+	}
 }
 
 func oneOperand(a *Assembler, insn *instruction, operand1 Operand) {
-	variant := insn.variants[condition{rm: operand1.Bits()}]
-	if variant != nil {
-		insn = variant
+	variant := insn.variants[[2]condition{{rm: operand1.Bits()}}]
+	if variant == nil {
+		a.ReportError(errors.New("no variant defined for this operand combination"))
+		return
 	}
+	insn = variant
 	switch operand1.Bits() {
 	case 64:
 		operand1.Rex(a, Register{})
--- instructions.go
+++ instructions.go
@@ -5,26 +5,38 @@ var INC = &instruction{
 	opcode:   0xff,
 	assemble: oneOperand,
 	variants: variants{
-		{rm: 8}: {opcode: 0xfe},
+		{{rm: 8}}: {opcode: 0xfe},
+		{{rm: 16}}: {},
+		{{rm: 32}}: {},
+		{{rm: 64}}: {},
 	},
 }
 var DEC = &instruction{
 	Mnemonic: "dec",
 	opcode:   0xff,
 	assemble: oneOperand,
-	variants: variants{
-		{rm: 8}: {
-			opcode: 0xfe,
-			overrides: overrides{
-				"ModR/M r/m": byte(1),
-			},
-		},
-	},
 	overrides: overrides{
 		"ModR/M r/m": byte(1),
+	},
+	variants: variants{
+		{{rm: 8}}: {opcode: 0xfe},
+		{{rm: 16}}: {},
+		{{rm: 32}}: {},
+		{{rm: 64}}: {},
 	},
 }
 
 var MOV = ""
 var ADD = ""
 var RET = ""
+
+var allInstructions = []*instruction{
+	INC,
+	DEC,
+}
+
+func init() {
+	for _, insn := range allInstructions {
+		insn.initVariants()
+	}
+}
