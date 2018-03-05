1c0ff4f0c21282ed5a855b0ac07fb87cbdf85369
Tao Wen <taowen@gmail.com>
2018-03-05 21:01:14 +0800

    list all add opcodes

--- instructions.go
+++ instructions.go
@@ -28,44 +28,55 @@ var ADD = &instruction{
 	opcode:   0x00,
 	encoding: twoOperands,
 	variants: variants{
-		{{RM: 8}, {R: 8}}:      {},
-		{{RM: 16}, {R: 16}}:    {opcode: 0x01},
-		{{RM: 32}, {R: 32}}:    {opcode: 0x01},
-		{{RM: 64}, {R: 64}}:    {opcode: 0x01},
-		{{R: 8}, {RM: 8}}:      {opcode: 0x02},
-		{{R: 16}, {RM: 16}}:    {opcode: 0x03},
-		{{R: 32}, {RM: 32}}:    {opcode: 0x03},
-		{{R: 64}, {RM: 64}}:    {opcode: 0x03},
-		{{REG: "al"}, {IMM: 8}}: {opcode: 0x04, encoding: encodingI}, // 0x04 is shorter than 0x80 form
-		{{RM: 8}, {IMM: 8}}:    {opcode: 0x80},
+		{{RM: 8}, {R: 8}}:         {},
+		{{RM: 16}, {R: 16}}:       {opcode: 0x01},
+		{{RM: 32}, {R: 32}}:       {opcode: 0x01},
+		{{RM: 64}, {R: 64}}:       {opcode: 0x01},
+		{{R: 8}, {RM: 8}}:         {opcode: 0x02},
+		{{R: 16}, {RM: 16}}:       {opcode: 0x03},
+		{{R: 32}, {RM: 32}}:       {opcode: 0x03},
+		{{R: 64}, {RM: 64}}:       {opcode: 0x03},
+		{{REG: "al"}, {IMM: 8}}:   {opcode: 0x04, encoding: encodingI}, // 0x04 is shorter than 0x80 form
+		{{REG: "ax"}, {IMM: 16}}:  {opcode: 0x05, encoding: encodingI},
+		{{REG: "eax"}, {IMM: 32}}: {opcode: 0x05, encoding: encodingI},
+		{{RM: 8}, {IMM: 8}}:       {opcode: 0x80},
+		{{RM: 16}, {IMM: 16}}:     {opcode: 0x81},
+		{{RM: 32}, {IMM: 32}}:     {opcode: 0x81},
+		{{RM: 64}, {IMM: 64}}:     {opcode: 0x81},
+		{{RM: 16}, {IMM: 8}}:      {opcode: 0x83},
+		{{RM: 32}, {IMM: 8}}:      {opcode: 0x83},
+		{{RM: 64}, {IMM: 8}}:      {opcode: 0x83},
 	},
 }
 
 var MOV = &instruction{
 	mnemonic: "mov",
-	opcode: 0x88,
+	opcode:   0x88,
 	encoding: twoOperands,
 	variants: variants{
-		{{RM:8},{R:8}}:{},
-		{{RM:16},{R:16}}:{opcode: 0x89},
-		{{RM:32},{R:32}}:{opcode: 0x89},
-		{{RM:64},{R:64}}:{opcode: 0x89},
-		{{R:8},{RM:8}}:{opcode: 0x8a},
-		{{R:16},{RM:16}}:{opcode: 0x8b},
-		{{R:32},{RM:32}}:{opcode: 0x8b},
-		{{R:64},{RM:64}}:{opcode: 0x8b},
+		{{RM: 8}, {R: 8}}:   {},
+		{{RM: 16}, {R: 16}}: {opcode: 0x89},
+		{{RM: 32}, {R: 32}}: {opcode: 0x89},
+		{{RM: 64}, {R: 64}}: {opcode: 0x89},
+		{{R: 8}, {RM: 8}}:   {opcode: 0x8a},
+		{{R: 16}, {RM: 16}}: {opcode: 0x8b},
+		{{R: 32}, {RM: 32}}: {opcode: 0x8b},
+		{{R: 64}, {RM: 64}}: {opcode: 0x8b},
 	},
 }
 
 var RET = &instruction{
 	mnemonic: "ret",
-	opcode: 0xc3,
+	opcode:   0xc3,
 	encoding: zeroOperand,
 }
 
 var allInstructions = []*instruction{
 	INC,
 	DEC,
+	ADD,
+	MOV,
+	RET,
 }
 
 func init() {
